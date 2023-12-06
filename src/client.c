7bbd56bfd9d6543652bcdb6635f7561e6cbd46c0
Yifan Lin <yifanlin@Yifans-MacBook-Pro.local>
2023-12-05 21:50:57 -0500

    completed main loop for distributing tasks

--- src/client.c
+++ src/client.c
@@ -49,6 +49,7 @@ typedef struct {
     nio_t nio;
     int state;
     str_queue_t *tasks;
+    int *nrecv;
     // initialize / free by the routine
     vector_t resp_buf;  // response headers
     vector_t file_buf;  // file responded by the server
@@ -57,7 +58,7 @@ typedef struct {
 void build_request(Request *request, const char *filename, const char *hostname);
 void send_request(nio_t *nio, const char *filename);
 int routine(routine_data_t *data, bool terminate);
-bool init_routine(routine_data_t *conn, struct pollfd *pollfd, str_queue_t *tasks);
+bool init_routine(routine_data_t *conn, struct pollfd *pollfd, str_queue_t *tasks, int *nrecv);
 void clean_routine(routine_data_t *conn);
 
 int main(int argc, char *argv[]) {
@@ -71,11 +72,14 @@ int main(int argc, char *argv[]) {
     str_queue_t tasks;
     str_queue_init(&tasks);
 
+    // how many files have been received
+    int nrecv = 0;
+
     // set up connections and routine_data
     routine_data_t conns[N_CONNS];
     struct pollfd pollfds[N_CONNS];
     for (int i = 0; i < N_CONNS; i++) {
-        if (!init_routine(&conns[i], &pollfds[i], &tasks)) {
+        if (!init_routine(&conns[i], &pollfds[i], &tasks, &nrecv)) {
             fprintf(stderr, "connection %d failed\n", i);
             exit(1);
         }
@@ -90,11 +94,12 @@ int main(int argc, char *argv[]) {
 
     // request "dependency.csv"
     send_request(&conns[next_conn].nio, "dependency.csv");
+    next_conn = (next_conn + 1) % N_CONNS;
     if (conns[next_conn].nio.wbuf.size > 0)
         pollfds[next_conn].events |= POLLOUT;
-    next_conn = (next_conn + 1) % N_CONNS;
 
-    while (true) {
+    bool finished = false;
+    while (!finished) {
         int ret = poll(pollfds, N_CONNS, CONNECTION_TIMEOUT);
         if (ret < 0) {
             fprintf(stderr, "poll: %d, %s\n", errno, strerror(errno));
@@ -105,28 +110,73 @@ int main(int argc, char *argv[]) {
                 // connection terminated by server unexpectedly
                 clean_routine(&conns[i]);
                 // try to reconnect once
-                if (!init_routine(&conns[i], &pollfds[i], &tasks)) {
+                if (!init_routine(&conns[i], &pollfds[i], &tasks, &nrecv)) {
                     fprintf(stderr, "connection %d failed\n", i);
                     exit(1);
                 }
             }
+
             if (pollfds[i].revents & POLLIN) {
                 int status = routine(&conns[i], false);
                 if (status == FINISH) {
                     // server closed its read end unexpectedly
                     clean_routine(&conns[i]);
                     // try to reconnect once
-                    if (!init_routine(&conns[i], &pollfds[i], &tasks)) {
+                    if (!init_routine(&conns[i], &pollfds[i], &tasks, &nrecv)) {
                         fprintf(stderr, "connection %d failed\n", i);
                         exit(1);
                     }
+                } else {
+                    if (graph.size == 0 && nrecv == 1) {
+                        // received dependency.csv
+                        nrecv = 0;
+                        int fd = open("./www/dependency.csv", O_RDONLY);
+                        rio_t rio;
+                        rio_readinitb(&rio, fd);
+                        char line[MAX_LINE];
+                        char val[MAX_LINE] = "\0", key[MAX_LINE] = "\0";
+                        while (rio_readlineb(&rio, line, MAX_LINE) > 0) {
+                            sscanf(line, "%[^,],%s", val, key);
+                            graph_insert(&graph, key, val);
+                        }
+                        // push tasks to task queue
+                        key_file_t *key_file = graph_find(&graph, "");
+                        for (value_file_t *val_file = key_file->values; val_file != NULL; val_file = val_file->next)
+                            str_queue_push(&tasks, val_file->filename);
+                    } else if (nrecv == graph.nfiles) {
+                        // received all files
+                        finished = true;
+                        break;
+                    } else {
+                        // check if there are tasks to assign
+                        while (tasks.size > 0) {
+                            char *task = str_queue_pop(&tasks);
+                            send_request(&conns[next_conn].nio, task);
+                            free(task);
+                            next_conn = (next_conn + 1) % N_CONNS;
+                        }
+                    }
                 }
             }
+
             if (pollfds[i].revents & POLLOUT)
                 nio_writeb(&conns[i].nio, NULL, 0);
         }
+        if (finished)
+            break;
 
+        for (int i = 0; i < N_CONNS; i++) {
+            if (conns[i].nio.wbuf.size > 0)
+                pollfds[i].events |= POLLOUT;
+            else
+                pollfds[i].events &= (~POLLOUT);
+        }
     }
+
+    str_queue_free(&tasks);
+    for (int i = 0; i < N_CONNS; i++)
+        clean_routine(&conns[i]);
+    graph_free(&graph);
 
     return 0;
 }
@@ -163,13 +213,14 @@ void build_request(Request *request, const char *filename, const char *hostname)
     request->valid = true;
 }
 
-bool init_routine(routine_data_t *conn, struct pollfd *pollfd, str_queue_t *tasks) {
+bool init_routine(routine_data_t *conn, struct pollfd *pollfd, str_queue_t *tasks, int *nrecv) {
     int fd = open_clientfd(hostname, port);
     if (fd < 0)
         return false;
     nio_init(&conn->nio, fd);
     conn->state = 0;
     conn->tasks = tasks;
+    conn->nrecv = nrecv;
     pollfd->fd = fd;
     pollfd->events = POLLIN | POLLHUP | POLLERR;
     pollfd->revents = 0;
@@ -212,7 +263,7 @@ int routine(routine_data_t *data, bool terminate) {
 
         if (data->state == 3) {
             // read server response
-
+            return YIELD;
         }
     }
 }
